9b2dde9a8552bbea9d409c88ef86aca8ce74ec22
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-08-06 20:44:31 +0200

    5. Implementing form validation

--- server/users.ts
+++ server/users.ts
@@ -2,13 +2,20 @@
 
 import { auth } from "@/lib/auth";
 
-export const signIn = async () => {
-  await auth.api.signInEmail({
-    body: {
-      email: "user@email.com",
-      password: "password",
-    },
-  });
+export const signIn = async (email: string, password: string) => {
+  try {
+    await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+    });
+
+    console.log("Signed in successfully");
+  } catch (error) {
+    console.error("Sign-in error:", error);
+    throw new Error("Failed to sign in");
+  }
 };
 
 export const signUp = async () => {
